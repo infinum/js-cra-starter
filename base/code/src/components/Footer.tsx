5123e2e1b9c8075c67c1152561c8087a16354ba7
Darko Kukovec <DarkoKukovec@users.noreply.github.com>
2019-05-10 18:06:15 +0200

    Use an interface for the component props
    Co-Authored-By: Andrei Zvonimir Crnković <andrei@infinum.co>

--- base/code/src/components/Footer.tsx
+++ base/code/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
-export const Footer: React.FC<{ className?: string }> = ({ className }) => (
+interface IFooterProps {
+  className?: string;
+}
+
+export const Footer: React.FC<IFooterProps> = ({ className }) => (
   <div className={className}>Made with <span role="img" aria-label="love">❤️</span> by Infinum</div>
 );
