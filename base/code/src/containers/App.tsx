8e07095424e2a37a382fd4b1f7f3956ea409b4e4
veksi <vedran.serences@infinum.hr>
2019-09-04 23:20:15 +0200

    Implement App as FC

--- base/code/src/containers/App.tsx
+++ base/code/src/containers/App.tsx
@@ -1,5 +1,5 @@
 import { css, keyframes } from 'emotion';
-import * as React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 import logo from '../assets/logo.svg';
 import { Footer } from '../components/Footer';
@@ -18,9 +18,18 @@ const appClass = css`
   text-align: center;
 `;
 
-const logoClass = css`
-  animation: ${logoSpinKeyframes} infinite 20s linear;
-  height: 40vmin;
+const logoClass = (animationSpeed: number) => css`
+  animation: ${logoSpinKeyframes} infinite ${animationSpeed}s linear;
+  height: 30vmin;
+`;
+
+const buttonClass = css`
+  padding: 10px;
+  margin: 5px;
+`;
+
+const stateContaninerClass = css`
+  margin: 40px 0;
 `;
 
 const headerClass = css`
@@ -43,26 +52,43 @@ const footerClass = css`
   font-size: 16px;
 `;
 
-export class App extends React.Component {
-  public render() {
-    return (
-      <div className={appClass}>
-        <main className={headerClass}>
-          <img src={logo} className={logoClass} alt="logo" />
-          <p>
-            Edit <code>src/containers/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className={linkClass}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Footer className={footerClass} />
-        </main>
-      </div>
-    );
+export const App: FunctionComponent<{}> = () => {
+  const [animationSpeed, setAnimationSpeed] = useState(40);
+
+  const onButtonClick = (action: string) => {
+    return () => {
+      if (action === '+' && animationSpeed > 5) {
+        setAnimationSpeed(animationSpeed - 5);
+      } else if (action === '-') {
+        setAnimationSpeed(animationSpeed + 5);
+      }
+    }
   }
+
+  return (
+    <div className={appClass}>
+      <main className={headerClass}>
+        <img src={logo} className={logoClass(animationSpeed)} alt="logo" />
+        <p>
+          Edit <code>src/containers/App.tsx</code> and save to reload.
+        </p>
+        <div className={stateContaninerClass}>
+          <span>ANIMATION SPEED</span>
+          <div>
+            <button className={buttonClass} onClick={onButtonClick('+')}>+</button>
+            <button className={buttonClass} onClick={onButtonClick('-')}>-</button>
+          </div>
+        </div>
+        <a
+          className={linkClass}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Footer className={footerClass} />
+      </main>
+    </div>
+  );
 }
